13ac82bac95dcc95facdbbde7619e5217aa791e3
EduVieira0101 <eduzinhovieira17@gmail.com>
2024-12-09 22:24:28 -0300

    PROJETO JAVA SCRIPT AULA 3

---
+++ teste.js
@@ -0,0 +1,13 @@
+var nome = "Roberto"  //Global
+//let sobrenome = " "
+function ColocaSobrenome() {`
+    var sobrenome = "Carlos"   
+    console.log("Olá " + nome + " " + sobrenome)
+}
+function func1(){
+console.log ("Olá " + nome + "" + sobrenome)
+}
+ColocaSobrenome
+    console.log ("Olá " + nome + "" + sobrenome)
+    func1()
+
